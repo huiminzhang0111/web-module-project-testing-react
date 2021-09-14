9455b610f239466b1c1b3213e0119a6851050380
Huimin <huiminzhang0111@hotmail.com>
2021-09-13 17:44:34 -0700

    episode testing done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -2,30 +2,46 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+
 const testEpisode = {
     id:1,
     name: "",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "a specific summary statement",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
     //Add in approprate test data structure here.
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>);
 });
 
+
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode={testEpisode}/>);
+    const summaryStatement = screen.queryByText(/a specific summary statement/i);
+    expect(summaryStatement).toBeInTheDocument();
+    expect(summaryStatement).not.toBeNull();
+    expect(summaryStatement).toBeTruthy()
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+    const alternativeImage = screen.queryByAltText('./stranger_things.png');
+    expect(alternativeImage).toBeTruthy()
+
 })
 
 //Tasks
